4513fe02799d5731bb6801b4112e660d3651a056
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-02-17 16:04:53 +0300

    test_class.h

---
+++ chapter_2/exr_2.42/test_class.h
@@ -0,0 +1,12 @@
+#include "test_class2.h"
+
+class Test_class{
+public:
+    Test_class(int x, int y);
+    int function();
+private:
+    int data1;
+    int data2;
+    int data3;
+    Test_class2 class2;
+};
