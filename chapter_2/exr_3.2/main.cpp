c5998876bd837b33215d346900b7f743a62aa8e8
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-02-18 12:12:02 +0300

    Create main.cpp

---
+++ chapter_2/exr_3.2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+    string s;
+    cout << "Enter two words with the space:" << endl;
+    cin >> s;
+    cout << "As result only one word:\n" << s << endl;
+}
