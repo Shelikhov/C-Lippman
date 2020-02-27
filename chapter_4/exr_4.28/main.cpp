5ca4d2de52bcf55ca917f2d40da3ff837789ab4b
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-02-27 11:27:21 +0300

    sizeof

---
+++ chapter_4/exr_4.28/main.cpp
@@ -0,0 +1,13 @@
+#include<vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "char size:\t" << sizeof(char) << endl;
+    cout << "int size:\t" << sizeof(int) << endl;
+    cout << "double size:\t" << sizeof(double) << endl;
+    cout << "unsigned size:\t" << sizeof(unsigned) << endl;
+    cout << "long size:\t" << sizeof(long) << endl;
+    cout << "long long size:\t" << sizeof(long long) << endl;
+}
