e9d8f4a74d544cb319a8674d0db96b6a59105b6e
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-02-29 14:16:26 +0300

    SWITCH
    To count vowel letters in the text.

---
+++ chapter_5/exr_5.9/main.cpp
@@ -0,0 +1,22 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string str;
+    char ch;
+    int cnt = 0;
+    cout << "Enter text:\n";
+    getline(cin, str);
+    string::iterator it = str.begin();
+    while(it != str.end()){
+        ch = tolower(*it);
+        switch (ch){
+            case 'e': case 'y': case 'u': case 'i': case 'o': case 'a': ++cnt;
+        default:{}
+        }
+        ++it;
+    }
+    cout << "Result: " << cnt;
+}
