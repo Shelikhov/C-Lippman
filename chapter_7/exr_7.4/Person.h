389b06f892ac31b927d4311f118c1798e7e1ee79
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-07 00:38:27 +0300

    Create Person.h

---
+++ chapter_7/exr_7.4/Person.h
@@ -0,0 +1,17 @@
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+#ifndef PERSON_H
+#define PERSON_H
+
+class Person{
+public:
+    Person();
+private:
+    string name;
+    string address;
+};
+
+#endif
