22623be96ec6db4d7b086eaa8ffad11047fea4e8
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-08 15:50:07 +0300

    Update screen.h
    To change return type of function move for using for example expresion such as (myWin.move(3, 8).insert(word)).

--- chapter_7/exr_7.23/screen.h
+++ chapter_7/exr_7.23/screen.h
@@ -14,7 +14,7 @@ public:
     Screen(string::size_type, string::size_type, char);
     char &get();//To get cursor value.
     char &get(string::size_type r, string::size_type c);//To get value in the specified coordinates (r is row, c is column).
-    void move(string::size_type, string::size_type);//To move cursor in the specified coordinates.
+    Screen &move(string::size_type, string::size_type);//To move cursor in the specified coordinates.
     void showWin(ostream &);//To show window content.
     void insert(string &);//To insert text into window.
 private:
