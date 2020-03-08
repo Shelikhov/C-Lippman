92f75849ec34b11e6d9b4e6d92d17894f9577c38
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-08 16:23:44 +0300

    exr_7.27

--- chapter_7/exr_7.23/main.cpp
+++ chapter_7/exr_7.23/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main(){
     Screen myWin(24, 50, 'c');//To create window.
+    const Screen myWin2(23, 34, 'H');
     myWin.showWin(cout);//To show window.
     cout << myWin.get();//To show cursor value.
     
@@ -13,7 +14,9 @@ int main(){
     string word;
     getline(cin, word);
 
-    myWin.move(2, 7).insert(word);//To write text in the window.
+    myWin.move(2, 7).insert(word).showWin(cout);//To write text in the window.
+    cout << "\n\n\n";
+    myWin2.showWin(cout);
     
     myWin.showWin(cout);
     cout << myWin.get();
