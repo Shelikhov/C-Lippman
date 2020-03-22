84fdd3ae0b28f96dead68ec99d701c4a8b315dfc
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-22 17:38:45 +0300

    iostream_iterator

---
+++ chapter_10/exr_10.31/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<iterator>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+    istream_iterator<int> itIn(cin), eof;
+    ostream_iterator<int> itOut(cout, "\n");
+    vector<int> vec(itIn, eof);
+    sort(vec.begin(), vec.end());
+    unique_copy(vec.begin(), vec.end(), itOut);
+}
