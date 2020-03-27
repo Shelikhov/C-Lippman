a66aa7c40d43d350e75b8f8728c589d21374b485
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-27 22:28:44 +0300

    operator=

---
+++ chapter_13/exr_13.8(second example)/hasptr.h
@@ -0,0 +1,19 @@
+#ifndef HASPTR_H
+#define HASPTR_H
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Hasptr{
+public:
+    Hasptr(int, const string&);//Initialization constructor.
+    Hasptr(const Hasptr&);//Copy constructor.
+    Hasptr &operator=(const Hasptr &);//Asignment operator.
+    int val1;
+    string *val2;
+    void print();
+};
+
+#endif
