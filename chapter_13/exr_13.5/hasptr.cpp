0a29b369049e43782bd6c6c0953bf448f67f488c
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-27 22:08:35 +0300

    copy constructor

---
+++ chapter_13/exr_13.5/hasptr.cpp
@@ -0,0 +1,11 @@
+#include "hasptr.h"
+
+Hasptr::Hasptr(const Hasptr &obj): val1(obj.val1), val2(new string(*(obj.val2))){
+}
+
+Hasptr::Hasptr(int val, const string &str = string()): val1(val), val2(new string(str)){
+}
+
+void Hasptr::print(){
+    cout << "val1:\t" << val1 << "\nval2:\t" << *val2 << "\n";
+}
