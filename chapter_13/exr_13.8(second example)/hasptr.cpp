1a01989db75bee5ef996ef3af36a9bd68d5932d3
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-27 22:29:10 +0300

    operator=

---
+++ chapter_13/exr_13.8(second example)/hasptr.cpp
@@ -0,0 +1,16 @@
+#include "hasptr.h"
+
+Hasptr::Hasptr(const Hasptr &obj): val1(obj.val1), val2(new string(*(obj.val2))){
+}
+
+Hasptr::Hasptr(int val, const string &str = string()): val1(val), val2(new string(str)){
+}
+
+Hasptr &Hasptr::operator=(const Hasptr &obj){
+    val1 = obj.val1;
+    val2 = new string(*(obj.val2));
+}
+
+void Hasptr::print(){
+    cout << "val1:\t" << val1 << "\nval2:\t" << *val2 << "\n";
+}
