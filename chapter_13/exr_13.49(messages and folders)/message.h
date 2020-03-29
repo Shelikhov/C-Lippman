a13c37dccba6f67e6266798bce9a270e18d71972
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-29 23:11:14 +0300

    Adding move constructor and move operatpr=.

---
+++ chapter_13/exr_13.49(messages and folders)/message.h
@@ -0,0 +1,30 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include<string>
+#include<set>
+#include"folder.h"
+
+using namespace std;
+class Folder;
+
+class Message{
+public:
+    friend class Folder;
+    Message(string str);
+    Message(const Message &);
+    Message &operator=(const Message &);
+    Message(Message &&);//Move constructor.
+    Message &operator=(Message &&);//Move assignment operator.
+    ~Message();
+    void save(Folder &);//To add current message to certain folder.
+    void remove(Folder &);//To delete current message from certain folder.
+    void printAllFolders();//To show all folder in which this message.
+private:
+    string content;//Content of this message.
+    set<Folder *> folders;
+    void addMsgToFolders();//For copy constructor and operator=.
+    void remvMsgFromFolders();//For operator= and destructor.
+};
+
+#endif
