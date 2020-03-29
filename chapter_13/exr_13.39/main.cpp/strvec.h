dbf76a3ccf0abadce005fc0b923df962f9457ede
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-29 18:35:13 +0300

    allocator

---
+++ chapter_13/exr_13.39/main.cpp/strvec.h
@@ -0,0 +1,31 @@
+#ifndef STRVEC_H
+#define STRVEC_H
+
+#include<utility>
+#include<string>
+#include<memory>
+#include<iostream>
+
+using namespace std;
+
+class StrVec{
+public:
+    StrVec();
+    StrVec(const StrVec &);
+    StrVec &operator=(const StrVec&);
+    void printAllElem();
+    void push_back(string &);//To put string to the end of array.
+private:
+    void free();//To free whole storage.
+    allocator<string> alloc;
+    pair<string*, string*> alloc_n_copy(const string *, const string *);
+    void check_n_alloc();//To check whether the exist a free storage for one new string.
+    size_t size();//Number of existing elements in array.
+    size_t capacity();//Capacity of whole string array.
+    void reallocate();//To allocate new capacity.
+    string *elements;//First existing element.
+    string *firstFree;//First free element after existing last element.
+    string *cap;//Last element into allocated array.
+};
+
+#endif
