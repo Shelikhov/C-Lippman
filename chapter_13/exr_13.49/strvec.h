a9034115810a153a17ef56b007fe589b08109948
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-29 22:31:40 +0300

    Adding move constructor and move operator=.

---
+++ chapter_13/exr_13.49/strvec.h
@@ -0,0 +1,34 @@
+#ifndef STRVEC_H
+#define STRVEC_H
+
+#include<utility>
+#include<string>
+#include<memory>
+#include<iostream>
+
+using namespace std;
+
+class StrVec{
+public:
+    StrVec();
+    StrVec(initializer_list<string>);
+    StrVec(const StrVec &);//Copy constructor.
+    StrVec &operator=(const StrVec&);//Copy assignment operator.
+    StrVec(StrVec &&)noexcept;//Move constructor.
+    StrVec &operator=(StrVec &&)noexcept;//Move assignment operator.
+    void printAllElem();
+    void push_back(string &);//To put string to the end of array.
+private:
+    void free();//To free whole storage.
+    allocator<string> alloc;
+    pair<string*, string*> alloc_n_copy(const string *, const string *);
+    void check_n_alloc();//To check whether the exist a free storage for one new string.
+    size_t size();//Number of existing elements in array.
+    size_t capacity();//Capacity of whole string array.
+    void reallocate();//To allocate new capacity.
+    string *elements;//First existing element.
+    string *firstFree;//First free element after existing last element.
+    string *cap;//Last element into allocated array.
+};
+
+#endif
