5c102ef4c46f035668c1a9278890b90ecfe3b203
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-30 21:01:45 +0300

    Adding class message.cpp.

---
+++ chapter_14/exr_14.6/message.cpp
@@ -0,0 +1,34 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include<string>
+#include<set>
+#include"folder.h"
+
+using namespace std;
+class Folder;
+
+class Message{
+public:
+    friend ostream &operator<<(ostream &threadOut, Message &mes);
+    friend class Folder;
+    Message(string str);
+    Message(const Message &);
+    Message &operator=(const Message &);
+    Message(Message &&);//Move constructor.
+    Message &operator=(Message &&);//Move assignment operator.
+    bool operator==(const Message &);
+    bool operator!=(const Message &);
+    //ostream &operator<<(ostream &, const Message &);
+    ~Message();
+    void save(Folder &);//To add current message to certain folder.
+    void remove(Folder &);//To delete current message from certain folder.
+    void printAllFolders();//To show all folder in which this message.
+private:
+    string content;//Content of this message.
+    set<Folder *> folders;
+    void addMsgToFolders();//For copy constructor and operator=.
+    void remvMsgFromFolders();//For operator= and destructor.
+};
+
+#endif
