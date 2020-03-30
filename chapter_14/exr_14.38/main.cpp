de54401ad32e93f12d343d89f393e1754049625d
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-31 00:16:16 +0300

    operator()

---
+++ chapter_14/exr_14.38/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include"wordsize.h"
+#include<fstream>
+#include<sstream>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    ifstream file("C:/Users/yashelik/Documents/file_read.txt");
+    size_t beg = 1, end = 5;
+    size_t count = 0;
+    string line, word;
+    WordSize obj(beg, end);
+    while(getline(file, line)){
+        istringstream sstr(line);
+        while(sstr >> word){
+            if(obj(word))
+                ++count;
+        }
+    }
+    cout << count << "\n";
+    file.close();
+}
