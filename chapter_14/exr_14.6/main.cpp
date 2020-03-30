d2ede2a994ff78fa1177cf3974b9dd418d9ef55a
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-30 21:28:50 +0300

    operator>>

--- chapter_14/exr_14.6/main.cpp
+++ chapter_14/exr_14.6/main.cpp
@@ -5,13 +5,15 @@
 
 using namespace std;
 ostream &operator<<(ostream &, Message &);
+istream &operator>>(istream &, Message &);
 
 int main(){
-    Message msg1("mes1"), msg2("mes2");
+    Message msg1("mes1"), msg2("mes2"), msg3;
     Folder fld1("folder1"), fld2("folder2");
     msg1.save(fld1);
     msg2.save(fld2);
-    cout << msg1 << msg2;
+    cin >> msg3;
+    cout << msg1 << msg2 << msg3;
     if(msg1 != msg2)
         cout << "Contents in the msg1 and msg2 are equal!\n";
     else
@@ -24,4 +26,11 @@ ostream &operator<<(ostream &threadOut, Message &mes){
         threadOut << el->name << "\t";
     threadOut << "\n";
     return threadOut;
+}
+
+istream &operator>>(istream &threadIn, Message &mes){
+    threadIn >> mes.content;
+    if(!threadIn)
+        mes = Message();
+    return threadIn;
 }
