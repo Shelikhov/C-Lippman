a9c0d00e6a49f1a7def87558856d5b9244c84844
Shelikhov <49662564+Shelikhov@users.noreply.github.com>
2020-03-31 23:02:40 +0300

    Create quote.h

---
+++ chapter_15/exr_15.7/quote.h
@@ -0,0 +1,24 @@
+#ifndef QUOTE_H
+#define QUOTE_H
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Quote{
+public:
+    Quote() = default;
+    Quote(string, double);
+    string isbn();
+    unsigned count();
+    virtual double price(unsigned);
+    virtual ~Quote() = default;
+private:
+    string bookName;
+    unsigned bookCount;
+protected:
+    double bookPrice;
+};
+
+#endif
